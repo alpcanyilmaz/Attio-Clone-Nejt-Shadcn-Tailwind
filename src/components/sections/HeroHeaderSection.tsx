f1ef55507f2ffd2e1bb4c04158d68b10184f5805
alpcanyilmaz <alpcanyilmazz@gmail.com>
2024-09-30 17:35:30 +0300

    added hero youtube modal and youtube iframe on HeroSection

--- src/components/sections/HeroHeaderSection.tsx
+++ src/components/sections/HeroHeaderSection.tsx
@@ -1,21 +1,18 @@
-import { ChevronRight } from 'lucide-react'
-import React from 'react'
+import { ChevronRight } from "lucide-react";
+import React from "react";
 
 function HeroHeaderSection() {
   return (
-    <div className='flex justify-center max-w-fit mx-auto py-1 px-1 bg-[#F4F5F6] rounded-full mt-10'>
-        <div className='bg-primary rounded-full px-3 py-1 text-white'>
-            New
-        </div>
-        <span className='px-1 py-1'>
-            How Pallets uses Attio to power their GTM
-        </span>
-        <div className='flex justify-center items-center'>
-
-        <ChevronRight className='text-primary' />
-        </div>
+    <div className="flex justify-center max-w-fit mx-auto py-1 px-1 bg-[#F4F5F6] rounded-full mt-10">
+      <div className="bg-primary rounded-full px-3 py-1 text-white">New</div>
+      <span className="px-1 py-1">
+        How Pallets uses Attio to power their GTM
+      </span>
+      <div className="flex justify-center items-center">
+        <ChevronRight className="text-primary" />
+      </div>
     </div>
-  )
+  );
 }
 
-export default HeroHeaderSection
+export default HeroHeaderSection;
--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -1,12 +1,38 @@
-import React from 'react'
-import HeroHeaderSection from './HeroHeaderSection'
+import React from "react";
+import HeroHeaderSection from "./HeroHeaderSection";
+import MainButton from "../common/MainButton";
+import { HeroYoutubeModal } from "../modals/HeroYoutubeModal";
 
 function HeroSection() {
   return (
-    <section>
+    <section className="max-w-[1250px] mx-auto">
       <HeroHeaderSection />
+      <div className="mt-10 flex flex-col gap-10 mx-auto">
+        <h1 className="text-6xl md:text-8xl font-bold text-center text-[#1D1F20]">
+          Customer <br /> relationship magic.
+        </h1>
+        <span className="text-[20px] md:text-[22px] text-center max-w-[800px] mx-auto px-10 text-[#31373D]">
+          Powerful, flexible and data-driven, Attio makes it easy <br /> to
+          build the exact CRM your business needs.
+        </span>
+        <div className="flex gap-10 justify-center px-10">
+          <MainButton
+            text="Start for free"
+            size="small"
+            classes="border-none rounded-[12px]"
+          />
+          <MainButton
+            text="Talk to sales"
+            size="small"
+            classes="bg-white border-slate-50 text-[#31373D]"
+          />
+        </div>
+        <div className="flex justify-center w-full">
+          <HeroYoutubeModal />
+        </div>
+      </div>
     </section>
-  )
+  );
 }
 
-export default HeroSection
+export default HeroSection;
